1315d9d7d499d308607f72bd28d9804db3171131
lauravalen <valentimlaura03@gmail.com>
2026-05-07 15:25:10 -0300

    [add] Backend

---
+++ backend/src/main/java/com/fatec/bndes/BndesApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.bndes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BndesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BndesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fatec/bndes/BndesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.bndes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BndesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
